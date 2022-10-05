f9241a0934a9177fba4ecaef4813062a8a9e4d64
rahul576 <rahul.007.shrivastava@gmail.com>
2022-10-06 00:56:23 +0530

    initial project commit

---
+++ src/test/java/com/rahul/jsontoprotobuffplugin/JsontoprotobuffpluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.jsontoprotobuffplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsontoprotobuffpluginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
